e3af95a072d91bfb3cadafbe00e7be4b7675df3e
Josias <87546094+Josyas@users.noreply.github.com>
2022-01-18 22:44:13 -0300

    arquivos projeto

---
+++ ContaBancariaExcecao/ContaBancariaExcecao/Entities/Account.cs
@@ -0,0 +1,42 @@
+﻿using ContaBancariaExcecao.Entities.Exceptions;
+using System.Globalization;
+namespace ContaBancariaExcecao.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+        public double ToWithDraw { get; set; }
+        
+
+        public Account(int number, string holder, double balance, double withDrawLimit, double toWithDraw)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+            ToWithDraw = toWithDraw;
+          
+        }
+                       
+        public void WithDraw(double toWithDraw)
+        {
+           if (ToWithDraw > WithDrawLimit)
+           {
+              throw new DomainException("The amount exceeds withdraw limit.");
+           }
+           if (ToWithDraw > Balance)
+           {
+                throw new DomainException("Not enough balance.");
+           }
+           ToWithDraw = toWithDraw - Balance;                                                                     
+        }
+
+        public override string ToString()
+        {
+            return "New Balance: " + ToWithDraw.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancariaExcecao/ContaBancariaExcecao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ContaBancariaExcecao.Entities;
+using ContaBancariaExcecao.Entities.Exceptions;
+using System.Globalization;
+
+
+namespace ContaBancariaExcecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+
+                Console.WriteLine();
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Enter amount for withdraw: ");
+                double toWithDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, limit, toWithDraw);
+                account.WithDraw(toWithDraw);
+                Console.WriteLine(account);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdarw error: " + e.Message);
+            }
+        }
+    }
+}
